19c875a76a885949bae41b3d2014934260e7ac4b
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-17 08:35:53 +0530

    master-commit-02

--- JunitSimpleProject/src/main/java/com/it/Rough.java
+++ JunitSimpleProject/src/main/java/com/it/Rough.java
@@ -3,10 +3,7 @@ package com.it;
 public class Rough {
 
 	public static void main(String[] args) {
-         System.out.println("hi");
-         System.out.println("hello");
-         System.out.println("h r u");
-         System.out.println("guys");
+         
 	}
 
 }
