690b0205681cf4da33921fcd8ba30849957dc77e
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-17 09:10:58 +0530

    master-branch-commit-06

--- JunitSimpleProject/src/main/java/com/it/Rough.java
+++ JunitSimpleProject/src/main/java/com/it/Rough.java
@@ -6,7 +6,7 @@ public class Rough {
          System.out.println("hi");
          System.out.println("hello");
          System.out.println("h r u");
-         System.out.println("guys");
+         System.out.println("guys"); 
 	}
 
 }
