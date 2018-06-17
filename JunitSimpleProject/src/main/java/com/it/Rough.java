6541407a62669683009a39e34fcd6949c021ff03
BH04954 <Rajashekhar.Thota@macys.com>
2018-06-17 08:09:16 +0530

    branch-01 commit-nth

--- JunitSimpleProject/src/main/java/com/it/Rough.java
+++ JunitSimpleProject/src/main/java/com/it/Rough.java
@@ -6,6 +6,7 @@ public class Rough {
 
 		System.out.println("newly added class in branch-01");
 		System.out.println("****This is sunday morning 8:01 am***");
+		System.out.println(" hello hi how are you");
 	}
 
 }
